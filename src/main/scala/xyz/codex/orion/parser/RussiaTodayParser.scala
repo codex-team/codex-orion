786c0f71b14cfb0c190ce0dac4c471ae66b17235
Nostr <ntpcp@yandex.ru>
2015-11-30 20:45:28 +0300

    Tmp version of RT parser

--- src/main/scala/xyz/codex/orion/parser/RussiaTodayParser.scala
+++ src/main/scala/xyz/codex/orion/parser/RussiaTodayParser.scala
@@ -1,5 +1,10 @@
 package xyz.codex.orion.parser
 
+import scalaj.http._
+import scala.xml._
+import java.net._
+import scala.io.Source
+
 /**
   *
   * @author Nostr
@@ -11,6 +16,17 @@ class RussiaTodayParser {
     */
   def run() {
     println("RT parser is now working!")
+    val response: HttpResponse[String] = Http("https://www.rt.com/rss/").asString
+
+    if (response.code == 200){
+      val xml = XML.loadString(response.body)
+
+      val items = xml \\ "item"
+      for (item <- items) {
+        println(item \\ "title")
+      }
+    }
+
   }
 
 }
