ddcfc3baaf2627086383a68184c8399af98ce089
Nostr <ntpcp@yandex.ru>
2015-12-01 20:45:56 +0300

    Actors for xml

--- src/main/scala/xyz/codex/orion/parser/RussiaTodayParser.scala
+++ src/main/scala/xyz/codex/orion/parser/RussiaTodayParser.scala
@@ -1,17 +1,54 @@
 package xyz.codex.orion.parser
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSelection, ActorSystem}
 import akka.event.Logging
-import xyz.codex.orion.ArticleData
 import java.net.URL
+import xyz.codex.orion.ArticleData
+import xyz.codex.orion.ArticlePostProcessor.PostProcessArticle
 import scala.concurrent.Future
-import spray.http.{HttpResponse, HttpRequest}
+import spray.http.{HttpResponse, HttpRequest, StatusCode}
 import spray.client.pipelining._
 import scala.util.{Success, Failure}
-import akka.io.IO
-import scala.concurrent.duration._
+import scala.xml.XML
 
-case class RTParsingResult()
+case class RTParsingResult(result: HttpResponse) {}
+
+/*case class RTXMlParser extends Actor {
+
+  private val logger = Logging(context.system, this)
+  private val postProcessor: ActorSelection = context.actorSelection("../postProcessor")
+
+  def parseXml(result: HttpResponse): Option[ArticleData] = {
+    if (result.status == StatusCode.int2StatusCode(200)) {
+      println("OK")
+
+      val xml = XML.loadString(result.entity.data.asString)
+      val items = xml \\ "item"
+
+      for (item <- items) {
+        println(item \\ "title" text, item \\ "description" text)
+      }
+
+      //val (publisher, title, text) = ("", "", "")
+      return new ArticleData(publisher = "", title = "", text = "", url = new URL(basicUrl))
+    }
+    else {
+      println("ERR")
+      return new ArticleData(publisher = "", title = "", text = "", url = new URL(basicUrl))
+    }
+  }
+
+  override def receive: Receive = {
+    case RTParsingResult(result: HttpResponse) =>
+
+      val mayBeArticleData = parseXml(result)
+      mayBeArticleData match {
+        case Some(articleData) => postProcessor ! PostProcessArticle(articleData)
+        case None => logger.warning(s"Failed to parse RT article")
+      }
+  }
+}
+*/
 
 /**
   *
@@ -19,40 +56,50 @@ case class RTParsingResult()
   */
 case object RussiaTodayParser extends Parser{
 
-  def parseXml(result: HttpResponse): Unit = {
-    println(result.status)
+  val basicUrl : String = "https://www.rt.com/rss/"
+
+  def parseXml(result: HttpResponse): (String, Boolean) = {
+    if (result.status == StatusCode.int2StatusCode(200)) {
+      println("OK")
+
+      val xml = XML.loadString(result.entity.data.asString)
+      val items = xml \\ "item"
+
+      for (item <- items.take(2)) {
+        println(item \\ "title" text, item \\ "description" text)
+      }
+
+      return ("", true)
+    }
+    else {
+      println("ERR")
+      return ("", false)
+    }
   }
 
   override def parse(task: String): Option[ArticleData] = {
     println("RT parser is now working!")
-    //val response: HttpResponse[String] = Http("https://www.rt.com/rss/").asString
 
     implicit val system = ActorSystem("rt-parser-actor")
     import system.dispatcher
     val log = Logging(system, getClass)
 
     val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
-    val request = Get("http://maps.googleapis.com/maps/api/elevation/json?locations=27.988056,86.925278&sensor=false")
+    val request = Get(basicUrl)
     val response: Future[HttpResponse] = pipeline(request)
 
-    /*if (response.code == 200){
-      val xml = XML.loadString(response.body)
-
-      val items = xml \\ "item"
-      for (item <- items) {
-        println(item \\ "title")
-      }
-    }*/
-
     response onComplete {
-      case Success(result) =>
+      case Success(result: HttpResponse) =>
         parseXml(result)
+      case Success(somethingUnexpected) =>
+        log.warning("Success with unexpected data")
       case Failure(error) =>
-        log.warning("ERROR: ", error)
-
+        log.warning("Failure: ", error)
+      case _ =>
+        log.error("ERROR")
     }
 
-    val returnObject = ArticleData(publisher = "", title = "", text = "", url = new URL("https://www.rt.com/rss/"))
+    val returnObject = ArticleData(publisher = "", title = "", text = "", url = new URL(basicUrl))
     return Option(returnObject)
   }
 }
