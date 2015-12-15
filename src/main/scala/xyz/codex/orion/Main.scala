08c363acd635366beac7dc9cf0ffc34920423cbb
Nostr <ntpcp@yandex.ru>
2015-12-15 14:38:50 +0300

    Add scheduler for 15 seconds

--- src/main/scala/xyz/codex/orion/Main.scala
+++ src/main/scala/xyz/codex/orion/Main.scala
@@ -3,6 +3,7 @@ package xyz.codex.orion
 import akka.actor.{ActorRef, ActorSystem, Props}
 import xyz.codex.orion.parser.ArticlesParser
 
+import scala.concurrent.duration._
 /**
   *
   * @author eliseev
@@ -10,9 +11,12 @@ import xyz.codex.orion.parser.ArticlesParser
 object Main extends App{
   val system = ActorSystem("MySystem")
 
+  import system.dispatcher
+
   val articlesCrawlerDispatcher: ActorRef = system.actorOf(Props[ArticlesCrawlerDispatcher], "articlesCrawlerDispatcher")
   val articlesParser: ActorRef = system.actorOf(Props[ArticlesParser], "articlesParser")
   val articlesLoader: ActorRef = system.actorOf(Props[ArticlesLoaderActor], "articlesLoader")
 
-  articlesCrawlerDispatcher ! LaunchCrawlersToGetLinks
+  //articlesCrawlerDispatcher ! LaunchCrawlersToGetLinks
+  system.scheduler.schedule(0 seconds, 15 seconds, articlesCrawlerDispatcher, LaunchCrawlersToGetLinks)
 }
